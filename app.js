5804544f3e12ba872c1151724afa98113fe559af
damilolaadefenwa <ade24039@byui.edu>
2026-09-04 16:34:34 +0100

    remove a typo text

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 //Add a root route (GET /) that returns a short JSON response with a status code of 200.
 app.get("/", (req, res) => {
     return res.status(200).json({ message: "Hello, Web Services is running!" });
-});g
+});
 
 //Export the app.
 export default app;
