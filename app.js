a05c3546bce5efce6ffc9fca2e95bfa26b4b7677
damilolaadefenwa <ade24039@byui.edu>
2026-09-15 16:34:12 +0100

    Added Swagger documentation setup and generated swagger.json

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import router from "./src/router.js";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' with { type: 'json' };
 
 
 //Create an Express application instance.
@@ -7,6 +9,7 @@ const app = express();
 
 //Middleware to understand JSON data
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 
 //Add a root route (GET /) that returns a short JSON response with a status code of 200.
